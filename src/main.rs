76e05ac321097efed79242cdd0b3fcefb79f0243
Brent Carmer <bcarmer@gmail.com>
2016-11-25 11:36:13 -0800

    empty commit message

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
 use std::io;
 use std::io::{BufRead, Write};
 
+pub mod board;
+use board::Board;
+
 macro_rules! debug(
     ($($arg:tt)*) => { {
-        let r = writeln!(&mut io::stderr(), $($arg)*);
+        let mut stderr = io::stderr();
+        let r = writeln!(&mut stderr, $($arg)*);
         r.expect("failed printing to stderr");
+        let r = stderr.flush();
+        r.expect("failed flushing stderr");
+    } }
+);
+
+macro_rules! send(
+    ($($arg:tt)*) => { {
+        let mut stdout = io::stdout();
+        let mut stderr = io::stderr();
+        let s = format!($($arg)*);
+        stdout.write(s.as_str().as_bytes()).expect("failed printing to stdout");
+        stdout.write("\n".as_bytes()).expect("failed printing to stdout");
+        let debug = "sent message: \"".to_string() + &s + "\"\n";
+        stderr.write(debug.as_str().as_bytes()).expect("failed printing to stderr");
+        stderr.flush().expect("failed flushing stderr");
+        stdout.flush().expect("failed flushing stdout");
     } }
 );
 
 fn main() {
-
+    // main loop- recieving and sending messages to xboard
+    debug!("combustion started!");
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         let s = line.trim();
         debug!("recieved message: \"{}\"", s);
         println!("whats up!!!!!!");
+        match s {
+            "xboard" => send!(""),
+            "new" => {
+                let b = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+                debug!("{}", b);
+            }
+            _ => debug!("ignoring message"),
+        }
     }
 
 }
