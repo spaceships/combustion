04cb671d4e709a02cc3ceada806a59b0fe19b860
Brent Carmer <bcarmer@gmail.com>
2016-11-25 00:32:30 -0800

    script for logging to a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::io::{BufRead, Write};
+
+macro_rules! debug(
+    ($($arg:tt)*) => { {
+        let r = writeln!(&mut io::stderr(), $($arg)*);
+        r.expect("failed printing to stderr");
+    } }
+);
+
 fn main() {
-    println!("Hello, world!");
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let s = line.trim();
+        debug!("recieved message: \"{}\"", s);
+        println!("whats up!!!!!!");
+    }
+
 }
