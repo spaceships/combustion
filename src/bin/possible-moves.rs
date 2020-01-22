e062cd107a4990da9516f1b9dfc279330a79b5f2
Brent Carmer <bcarmer@galois.com>
2020-01-22 14:56:44 -0800

    work on possible moves

--- src/bin/possible-moves.rs
+++ src/bin/possible-moves.rs
@@ -1,4 +1,6 @@
 use combustion::board::Board;
+use combustion::moves::Move;
+use combustion::piece::Color;
 
 use std::env;
 use std::process::exit;
@@ -6,13 +8,24 @@ use std::process::exit;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        println!("Error: exactly one argument required. Exiting...");
+    if args.len() < 2 {
+        println!("Error: at least one argument required. Exiting...");
         exit(1);
     }
 
+    let mut board = if args[1] == "startpos" {
+        Board::initial()
+    } else {
+        Board::from_fen(&args[1]).unwrap()
+    };
 
-    let board = Board::from_fen(&args[1]).unwrap();
+    let mut to_move = Color::White;
+
+    for mv_str in args.iter().skip(2) {
+        // should be UCI format
+        let mv = Move::from_xboard_format(mv_str, &board).unwrap();
+        board = board.make_move(&mv).unwrap();
+    }
 
     for mv in &board.legal_moves().unwrap() {
         println!("{}", mv);
